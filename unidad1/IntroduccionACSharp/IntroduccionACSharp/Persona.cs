89f60a0447725bf45615e412e627bf838692175b
ingvega <isc.p.vega@gmail.com>
2023-01-27 09:09:13 -0600

    Integraciòn inicial de primer ejercicio con CSharp

---
+++ unidad1/IntroduccionACSharp/IntroduccionACSharp/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionACSharp
+{
+    internal class Persona
+    { 
+        public String Nombre { get; set; }
+
+        //private String nombre;
+        //public string getNombre() {
+        //    return nombre;
+        //}
+        //public void setNombre(string nombre) {
+        //    this.nombre = nombre;
+        //}
+    }
+}
