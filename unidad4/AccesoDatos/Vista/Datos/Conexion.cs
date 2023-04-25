6d8d3c6c282f6312edd1663b2461fe269b71f9ad
ingvega <isc.p.vega@gmail.com>
2023-04-25 08:57:23 -0600

    Ejercicio inicial de acceso a datos

---
+++ unidad4/AccesoDatos/Vista/Datos/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public static MySqlConnection conexion;
+        public bool Conectar() { 
+            conexion = new MySqlConnection();
+            conexion.ConnectionString = "sever=localhost;uid=root;pwd=123;database=northwind";
+            try
+            {
+                conexion.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+        }
+
+        public void Desconectar() { 
+        
+        }
+    }
+}
